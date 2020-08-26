a5974b3590eb663f4097acf43147ec85fed18713
Daniel Komesu <danielkomesu@gmail.com>
2020-08-25 21:49:25 -0300

    Add test template for comdexdown.fs

---
+++ tests/test_fs.py
@@ -0,0 +1,50 @@
+import unittest
+from unittest import mock
+
+from comexdown import fs
+
+
+class TestFS(unittest.TestCase):
+
+    def test_get_hash(self):
+        pass
+
+
+class TestDataDirectory(unittest.TestCase):
+
+    def test_path_aux(self):
+        pass
+
+    def test_path_trade(self):
+        pass
+
+    def test_update_aux(self):
+        pass
+
+    def test_update_trade(self):
+        pass
+
+    def test_update_exp(self):
+        pass
+
+    def test_update_imp(self):
+        pass
+
+    def test_update_exp_mun(self):
+        pass
+
+    def test_update_imp_mun(self):
+        pass
+
+    def test_update_aux_all(self):
+        pass
+
+    def test_create_index(self):
+        pass
+
+    def test_read_index(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
